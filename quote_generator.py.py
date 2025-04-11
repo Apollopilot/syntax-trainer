d92bebbd345a6dcf88855f8734aba34dfb0fa5df
Apollopilot <iamneveronline@gmail.com>
2025-04-11 04:07:50 -0700

    Add files via upload

---
+++ quote_generator.py.py
@@ -0,0 +1,16 @@
+import random
+
+# 1. Create a list of 5 Stoic quotes
+quotes = [
+    "Waste no more time arguing what a good man should be. Be one. – Marcus Aurelius",
+    "He who angers you conquers you. – Elizabeth Kenny",
+    “Fear keeps pace with hope. Both belong to a mind in suspense. To a mind in anxiety through thought of the future. - Seneca”,
+    “A persons caliber is revealed by difficult circumstances, and so, when a difficulty occurs, think of it as god pitting you, as a trainer might, against a tough training partner. Epictetus”
+    “Nothing, to my way of thinking, is a better proof of a well ordered mind than a man’s ability to stop just where he is and pass some time in his own company. - Seneca” # Add more...
+]
+
+# 2. Pick a random quote
+random_quote = random.choice(quotes) # your code here
+
+# 3. Print it out
+print(random_quote)# your code here)
