a36abb3bcfe82774e3d5799c0c9dd8eba5f6f42b
Apollopilot <iamneveronline@gmail.com>
2025-04-12 08:52:50 -0700

    Add files via upload
    In this version, if the user inputs something <3, the app will indicate "Password must be at least 3 characters", and goes back to the beginning of the while loop.

---  password_generator.py
+++  password_generator.py
@@ -1,14 +1,41 @@
 # password_generator.py
 import string
 import random
-letters = string.ascii_letters
-digits = string.digits
+letters = string.ascii_letters # shortcut that returns a string containing a-z, A-Z
+digits = string.digits # shortcut that returns string containing 1-9
 symbols = "!@#$%^&*()"
 
-all_chars = letters + digits + symbols 
+while True: #infinite loop
+    length = int(input( "Enter Password Length (minimum 3): ")) #integer to string with input
 
-length = int(input( "Enter Password Length "))
+    if length < 3:
+        print("Password must be at least 3 characters long ") #sends back to beginning of loop
+        
+    if length == "done":
+        exit 
+    else: 
+        break #exits loop if valid
 
-password = ''.join(random.choices(all_chars, k=length))
+    
+password_chars = [
+     random.choice(letters),
+     random.choice(digits),
+     random.choice(symbols)   
+    ]
+all_chars = letters + digits + symbols # concatenation 
+password_chars = random.choices(all_chars, k=length - 3) #string joining
 
-print("Generated password: ", password) 
+random.shuffle(password_chars) #shuffle to make it unpredictable
+
+password = ''.join(password_chars)
+
+print("Secured password: ", password) 
+
+## summary of concepts used
+
+# Module import
+# User input 
+# Type conversion ('int()')
+# Random selections ('random.choices(population, k=number)') function does 2 things What to choose from and how many things to choose (called k)
+# String joining (' ''.join()')
+# Variables, concatenation, function calls
